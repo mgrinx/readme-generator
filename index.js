5cab283a383d306e71a16c17ed0bbac0e421ce02
Misha Gringauze <mgringauze@gmail.com>
2020-05-21 16:42:05 -0700

    final readme fix

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const gen =async r=> {
 
     let contributors = ""
     for(let v of contributorsRaw) {
-        contributors += `<img align="left" src="${v.html_url}.png?size=24"><a href="${v.html_url}">${v.login}</a><br>\n`
+        contributors += `<img align="left" src="${v.html_url}.png?size=24"><a href="${v.html_url}">${v.login}</a><br>  \n`
     }
 
     let readme =
